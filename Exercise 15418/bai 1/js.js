1df0b5752e2bc76cca08fb8b51b31c55960fa3d4
tsmkevin815 <tsmkevin815@gmail.com>
2018-04-23 20:04:09 +0700

    first commit

---
+++ Exercise 15418/bai 1/js.js
@@ -0,0 +1,102 @@
+var currentTab = 0; 
+showTab(currentTab); 
+
+function validateEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+  }
+  function checkmail(a) {
+    var email = a.value;
+    if (validateEmail(email)) {
+      document.getElementById('emailError').innerHTML = '';
+    }
+    else {
+      document.getElementById('emailError').innerHTML = 'Wrong Email!';
+    }
+  }
+
+function showTab(n) {
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  
+  var x = document.getElementsByClassName("tab");
+ 
+  if (n == 1 && !validateForm()) return false;
+  
+  x[currentTab].style.display = "none";
+  
+  currentTab = currentTab + n;
+  
+  if (currentTab >= x.length) {
+    
+    document.getElementById("regForm").submit();
+    alert('Hoàn thành');
+  }
+  
+  showTab(currentTab);
+}
+
+function validateForm() {
+  
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  
+  for (i = 0; i < y.length; i++) {
+    
+    if (y[i].value == "") {
+      
+      y[i].className += " invalid";
+      
+      valid = false;
+    }
+  }
+  
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; 
+}
+
+function fixStepIndicator(n) {
+  
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  
+  x[n].className += " active";
+}
+
+function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    function checkmail(a) {
+      var email = a.value;
+      if (validateEmail(email)) {
+        document.getElementById('emailError').innerHTML = '';
+        flag = true;
+      }
+      else {
+        document.getElementById('emailError').innerHTML = 'Wrong Email!';
+        flag = false;
+      }
+      enable(flag, flag1);
+    }
